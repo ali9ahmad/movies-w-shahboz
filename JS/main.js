5753fe537717d68f633077915be6478c675213ca
ali9ahmad <temur.khan.dispatch@gmail.com>
2023-05-20 18:27:59 +0500

    genres generator is done

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,34 @@
 // GET ELEMENTS FROM HTML
-
-var elMovieSearchForm = document.querySelector(".js-search-form");
-var elMovieSearchInput = document.querySelector(".js-search-input");
-var elMovieSearchGenres = document.querySelector(".js-search-genres");
-var elMovieSearchFromYear = document.querySelector(".js-form-year");
-var elMovieSearchToYear = document.querySelector(".js-to-ye☺ar");
-var elMovieSearchSortSelect = elMovieSearchForm.querySelector(".js-sort-movies"):
-var elMovieList = document.querySelector(".js-movie__list");
+var elMoviesList = document.querySelector(".js-movies__list");
 var elMovieTemplate = document.querySelector(".js-movie__template").content;
 var newMoviesFragment = document.createDocumentFragment();
+// ----------------------------------------------------------------
+var elMovieSearchForm = document.querySelector(".js-search-form");
+var elMovieSearchInput = elMovieSearchForm.querySelector(".js-search-input");
+var elMovieSearchGenres = elMovieSearchForm.querySelector(".js-search-genres");
+var elMovieSearchFromYear = elMovieSearchForm.querySelector(".js-from-year");
+var elMovieSearchToYear = elMovieSearchForm.querySelector(".js-to-year");
+var elMovieSearchSortSelect =
+    elMovieSearchForm.querySelector(".js-sort-movies");
+
+
+/* console.log(elMoviesList, elMovieSearchForm, elMovieSearchInput, elMovieSearchGenres, elMovieSearchFromYear, elMovieSearchToYear, elMovieSearchSortSelect, elMovieTemplate); */
+
+
+// MOVIES GENRES GENERATED FUNCTION
+var genres = [];
+function generateGenres() {
+    movies.forEach(function (movie) {
+        // console.log(movie);
+        movie.Categories.split("|").forEach(function (category) {
+            // console.log(category);
+            if (!genres.includes(category)) {
+                genres.push(category);
+            }
+        });
+        genres.sort();
+    });
+}
+
+generateGenres();
 
