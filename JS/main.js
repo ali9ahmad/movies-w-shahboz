5f5e7918d24e48276f9ace0c7ea1ac91e0e61d58
ali9ahmad <temur.khan.dispatch@gmail.com>
2023-05-20 16:53:42 +0500

    elements added to js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,12 @@
 // GET ELEMENTS FROM HTML
+
+var elMovieSearchForm = document.querySelector(".js-search-form");
+var elMovieSearchInput = document.querySelector(".js-search-input");
+var elMovieSearchGenres = document.querySelector(".js-search-genres");
+var elMovieSearchFromYear = document.querySelector(".js-form-year");
+var elMovieSearchToYear = document.querySelector(".js-to-ye☺ar");
+var elMovieSearchSortSelect = elMovieSearchForm.querySelector(".js-sort-movies"):
+var elMovieList = document.querySelector(".js-movie__list");
+var elMovieTemplate = document.querySelector(".js-movie__template").content;
+var newMoviesFragment = document.createDocumentFragment();
+
